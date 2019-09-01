0681df069d2ef836ab1c11a3a7721b6cc3086ed5
Felipe Nery <felipenerypaulino@gmail.com>
2019-09-01 19:50:34 -0300

    Implementado reconhecimento de voz + exibição do gif e palavra

--- iLibra.ReconhecimentoVoz.Speech/Reconhecimento.cs
+++ iLibra.ReconhecimentoVoz.Speech/Reconhecimento.cs
@@ -1,14 +1,124 @@
-﻿using System.Speech.Synthesis;
+﻿using iLibra.ReconhecimentoVoz.Speech.Properties;
 using Microsoft.Speech.Recognition;
+using System.Drawing;
 using System.Globalization;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
 
 namespace iLibra.ReconhecimentoVoz.Reconhecimento
 {
-    public class Reconhecimento
+    public class Reconhece
     {
-        public Reconhecimento()
-        {
+        static CultureInfo culture;
+        static SpeechRecognitionEngine reconhecedor;
+        SpeechSynthesizer resposta;
+        private string[] palavras;
+        private Label lblResultado;
+        private PictureBox gif;
 
+        public Reconhece(Label ondeExibirResultado, PictureBox ondeExibirGif)
+        {
+            culture = new CultureInfo("pt-BR");
+            resposta = new SpeechSynthesizer();
+            lblResultado = ondeExibirResultado;
+            gif = ondeExibirGif;
+
+            palavras = new string[]
+            {
+                //colocar aqui as novas palavras
+                "oi","A","Acessibilidade","Aula","B","Bem_vindo_alunos","Boa_noite","Boa_tarde","Bom_dia","C","Computador","D","E","Engenharia","F","Faculdade","G","H","I","J","K","L","Libras","Linguagem","M","N","O","Ola","P","Programação","Q","R","S","T","Tchau","Tecnologia","U","V","W","X","Y","Z"
+            };
+
+            AjustaVolume();
+
+            InicializarReconhecimento();
+        }
+
+        private void AjustaVolume()
+        {
+            resposta.Volume = 100;
+            resposta.Rate = 4;
+        }
+
+        private void InicializarReconhecimento()
+        {
+            try
+            {
+                reconhecedor = new SpeechRecognitionEngine(culture);
+
+                var escolhas = new Choices();
+                escolhas.Add(palavras);
+
+                var construtorGramatica = new GrammarBuilder();
+                construtorGramatica.Append(escolhas);
+
+                ExecutaReconhecimento(gramatica: new Grammar(construtorGramatica));
+
+            }
+            catch (System.Exception e)
+            {
+
+                ExibeMensagemErro("Ops! " + e.Message);
+            }
+
+        }
+
+        private void ExibeMensagemErro(string mensagem)
+        {
+            lblResultado.Text = mensagem;
+            //MessageBox.Show(mensagem, "iLibra", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ExecutaReconhecimento(Grammar gramatica)
+        {
+            reconhecedor.RequestRecognizerUpdate();
+
+            reconhecedor.LoadGrammarAsync(gramatica);
+            reconhecedor.SpeechRecognized += ProcessarReconhecimento;
+            reconhecedor.SpeechDetected += Teste;
+
+            reconhecedor.SetInputToDefaultAudioDevice();
+
+            resposta.SetOutputToDefaultAudioDevice();
+
+            reconhecedor.RecognizeAsync(RecognizeMode.Multiple);
+            ExibirPalavraFalada("Detectando palavra....");
+        }
+
+        private void Teste(object sender, SpeechDetectedEventArgs e)
+        {
+            ExibirPalavraFalada("palavra não cadastrada");
+        }
+
+        private void ProcessarReconhecimento(object sender, SpeechRecognizedEventArgs e)
+        {
+            string palavraFalada = e.Result.Text;
+
+            if (palavras.Contains(palavraFalada))
+            {
+                ExibirPalavraFalada(palavraFalada, exibirGif: true);
+            }
+        }
+
+        private void ExibirPalavraFalada(string palavraFalada, bool exibirGif = false)
+        {
+            lblResultado.Text = palavraFalada;
+
+            if (exibirGif)
+            {
+
+                try
+                {
+                    gif.Image = (Image)Resources.ResourceManager.GetObject(palavraFalada, culture);
+
+                }
+                catch (System.Exception e)
+                {
+
+
+                }
+            }
         }
     }
 }
--- iLibra.ReconhecimentoVoz/ILibra.cs
+++ iLibra.ReconhecimentoVoz/ILibra.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using iLibra.ReconhecimentoVoz.Reconhecimento;
+using System;
 using System.Windows.Forms;
 
 namespace iLibra.ReconhecimentoVoz
 {
     public partial class ILibra : Form
     {
+        private Reconhece _reconhecimento;
         public ILibra()
         {
             InitializeComponent();
+            _reconhecimento = new Reconhece(ondeExibirResultado: LblPalavra, ondeExibirGif: ImgPalavra);
         }
 
         private void LblFechar_Click(object sender, EventArgs e)
