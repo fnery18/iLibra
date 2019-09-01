e4de882f73c8342111029be8500902862ceec462
Felipe Nery <felipenerypaulino@gmail.com>
2019-09-01 18:24:53 -0300

    Adicionado camada de reconhecimento + referencia a lib

---
+++ iLibra.ReconhecimentoVoz.Speech/Reconhecimento.cs
@@ -0,0 +1,14 @@
+﻿using System.Speech.Synthesis;
+using Microsoft.Speech.Recognition;
+using System.Globalization;
+
+namespace iLibra.ReconhecimentoVoz.Reconhecimento
+{
+    public class Reconhecimento
+    {
+        public Reconhecimento()
+        {
+
+        }
+    }
+}
